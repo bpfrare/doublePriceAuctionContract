9f9ea47946d7526c72e0f7bbf8e232f3e6565144
Bruno Frare <bpfrare@gmail.com>
2023-01-26 17:42:32 -0300

    Update trate test

--- test/testTrade.js
+++ test/testTrade.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 contract('DoublePriceAuction', (accounts) => {
   
-  for(let i=0;i<23;i++) {
+  for(let i=0;i<100;i++) {
     it(`should add ${i} bidder and ${i} Offer`, async () => {
       const doublePriceAuctionInstance = await DoublePriceAuction.deployed();
       await doublePriceAuctionInstance.registerBid(100, 59, {from: accounts[i]});
@@ -11,23 +11,21 @@ contract('DoublePriceAuction', (accounts) => {
       await doublePriceAuctionInstance.transfer(accounts[i], 10);
       await doublePriceAuctionInstance.registerOffer(i+1, 1, {from: accounts[i]});
     });
-    // if (i >= 25) {
-      it(`should run trade for ${i} bidder and ${i} Offer`, async () => {
-        let aux = ''
-        await new Promise(r => setTimeout(r, 1000*i));
-        const doublePriceAuctionInstance = await DoublePriceAuction.deployed();
-        let spend = new Date();
-        let tx = await doublePriceAuctionInstance.trade();
-        let spend_time = new Date().getTime() - spend.getTime()
-        aux = (i+1).toString() + ';' + spend_time.toString() + ';' + tx.receipt.gasUsed.toString() + '\r\n';
-        fs.appendFile('trade.csv', aux, err => {
-          if (err) {
-            console.error(err)
-            return
-          }
-        });
-
+    it(`should run trade for ${i} bidder and ${i} Offer`, async () => {
+      let aux = ''
+      await new Promise(r => setTimeout(r, 1000*i));
+      const doublePriceAuctionInstance = await DoublePriceAuction.deployed();
+      let spend = new Date();
+      let tx = await doublePriceAuctionInstance.trade();
+      let spend_time = new Date().getTime() - spend.getTime()
+      aux = (i+1).toString() + ';' + spend_time.toString() + ';' + tx.receipt.gasUsed.toString() + '\r\n';
+      fs.appendFile('./results/trade.csv', aux, err => {
+        if (err) {
+          console.error(err)
+          return
+        }
       });
-    // }
+
+    });
   }
 });
