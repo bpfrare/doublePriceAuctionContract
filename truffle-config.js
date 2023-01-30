9671d9e206bc55cf73c1650fd5b77ffcbaa2ea5b
Bruno Frare <bpfrare@gmail.com>
2023-01-30 11:45:09 -0300

    Setup sepolia network

--- truffle-config.js
+++ truffle-config.js
@@ -76,6 +76,14 @@ module.exports = {
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
+    sepolia: {
+      provider: () => new HDWalletProvider(mnemonic, `https://sepolia.infura.io/v3/${key}`),
+      network_id: 11155111,       // goerli's id
+      // gas: 5500000,        // goerli has a lower block limit than mainnet
+      confirmations: 2,    // # of confirmations to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     //
     // Useful for private networks
     // private: {
