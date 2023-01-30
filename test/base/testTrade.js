5b238f51cb6654c1237d17af832f65b91f7030ad
Bruno Frare <bpfrare@gmail.com>
2023-01-30 11:46:49 -0300

    fix base

--- test/base/testTrade.js
+++ test/base/testTrade.js
@@ -6,11 +6,13 @@ contract('DoublePriceAuction', (accounts) => {
   for(let i=0;i<100;i++) {
     it(`should add ${i} bidder and ${i} Offer`, async () => {
       const doublePriceAuctionInstance = await DoublePriceAuction.deployed();
+      // Register an higher price
       await doublePriceAuctionInstance.registerBid(100, 59, {from: accounts[i]});
       await doublePriceAuctionInstance.placeBid(10, {from: accounts[i]});
       await doublePriceAuctionInstance.transfer(accounts[i], 10);
       await doublePriceAuctionInstance.registerOffer(i+1, 1, {from: accounts[i]});
     });
+    
     it(`should run trade for ${i} bidder and ${i} Offer`, async () => {
       let aux = ''
       await new Promise(r => setTimeout(r, 1000*i));
@@ -19,7 +21,7 @@ contract('DoublePriceAuction', (accounts) => {
       let tx = await doublePriceAuctionInstance.trade();
       let spend_time = new Date().getTime() - spend.getTime()
       aux = (i+1).toString() + ';' + spend_time.toString() + ';' + tx.receipt.gasUsed.toString() + '\r\n';
-      fs.appendFile('./results/trade.csv', aux, err => {
+      fs.appendFile('./results/base/trade.csv', aux, err => {
         if (err) {
           console.error(err)
           return
