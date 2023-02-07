1bac2361ef069f59b4741f7538729ab46092faf9
Bruno Frare <bpfrare@gmail.com>
2023-02-07 09:15:01 -0300

    Testing same behavior

--- test/location/doublePriceLocation.js
+++ test/location/doublePriceLocation.js
@@ -4,7 +4,7 @@ const DoublePriceAuctionLocation = artifacts.require("DoublePriceAuctionLocation
 
 //Fatec: -23.162580, -45.794539
 
-contract('DoublePriceAuction', (accounts) => {
+contract('DoublePriceAuctionLocation', (accounts) => {
     it('should register the location', async () => {
         const doublePriceAuctionInstance = await DoublePriceAuctionLocation.deployed();
         await doublePriceAuctionInstance.setLocation(-23163217000, -45794390000);
@@ -53,6 +53,42 @@ contract('DoublePriceAuction', (accounts) => {
         
         let dist = await doublePriceAuctionInstance.getDistance.call(accounts[4], accounts[5]);
         assert.equal(dist.toNumber(), 72, 'The should be 72 meters');
+    });
+
+    it("should calc MCP", async () => {
+        /**
+            Value | Bid  | Offer
+            100   | 1000 |  500
+            125   | 910  |  650
+            150   | 830  |  770
+            175   | 760  |  870
+            200   | 700  |  950
+            225   | 650  | 1010
+
+            MCP = 157
+
+         */
+        const doublePriceAuctionInstance = await DoublePriceAuctionLocation.deployed();
+
+        IterableMapping
+        let values = [125, 100, 150, 175, 200, 225];
+        let bids = [910, 1000, 830, 760, 700, 650];
+        let offer = [650, 500, 770, 870, 950, 1010];
+        
+        for(let i = 0; i < values.length; i++) {
+            // register bids
+            await doublePriceAuctionInstance.registerBid(values[i], 2, {from: accounts[i]});
+            await doublePriceAuctionInstance.placeBid(bids[i], {from: accounts[i]});
+
+            // register offers
+            await doublePriceAuctionInstance.registerOffer(values[i],2, {from: accounts[values.length + i]});
+            await doublePriceAuctionInstance.placeOffer(offer[i], {from: accounts[values.length + i]});
+
+        }
+
+        let mcp = await doublePriceAuctionInstance.mcp.call();
+        console.log(mcp);
+        assert.equal(mcp, 150);
     });
 
 });
