cd410137fc730fc7183bfbbda63ae2f4e848dd3d
Bruno Frare <bpfrare@gmail.com>
2023-02-14 20:36:05 -0300

    test trade

--- exec/transfer.js
+++ exec/transfer.js
@@ -3,7 +3,7 @@ module.exports = async function(callback) {
     // var account = web3.eth.accounts.create();
     let accounts = await web3.eth.getAccounts();
     let promises = [];
-    for(let i=1;i<50;i++) {
+    for(let i=0;i<100;i++) {
       console.log(accounts[i]);
       promises.push(web3.eth.sendTransaction({
         from: accounts[0],
