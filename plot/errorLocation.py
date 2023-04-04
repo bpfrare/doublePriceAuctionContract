9d7887da01c6eb11838e81d34689d4ba5403af5d
Bruno Frare <bpfrare@gmail.com>
2023-04-03 21:07:02 -0300

    Add subplots

--- plot/errorLocation.py
+++ plot/errorLocation.py
@@ -6,14 +6,14 @@ def plot():
     plt.rcParams["figure.figsize"] = [7.50, 3.50]
     plt.rcParams["figure.autolayout"] = True
 
-    columns = ["Interação", "Solidity", "Javascript"]
+    columns = ["Interação", "Solidity", "Javascript", "Diferença"]
 
     df = pd.read_csv('./results/location/errorLoc.csv', delimiter=';',  names=columns)
+    
+    fig, axes = plt.subplots(nrows=1,ncols=2)
 
-    ax = df.set_index('Interação').plot(mark_right=False, grid=True)
-
-    ax.set_ylabel('Distância (m)')
-    ax.set_xlabel("Interações")
+    ax = df.plot(x='Interação', y='Solidity', mark_right=False, grid=True, ax=axes[0], subplots=True, xlabel="Interações", ylabel='Distância (m)')
+    ax = df.plot(x='Interação', y='Javascript', mark_right=False, grid=True, ax=axes[1], subplots=True, xlabel="Interações", ylabel='Distância (m)', style='r')
 
     dif = df['Javascript'] - df['Solidity']
     print(dif.max())
