20ce7dd8bbde130192cc20c7764e84a91447a3d3
Bruno Frare <bpfrare@gmail.com>
2023-04-28 20:29:08 -0300

    update values of test

--- test/base/testMCP.js
+++ test/base/testMCP.js
@@ -2,7 +2,7 @@ const DoublePriceAuction = artifacts.require("DoublePriceAuctionContract");
 const fs = require('fs');
 
 contract('DoublePriceAuction -> Test MCP', (accounts) => {
-  for(let i=0;i<1;i++) {
+  for(let i=0;i<100;i++) {
     it(`should add ${i} bidder and offers`, async () => {
       const doublePriceAuctionInstance = await DoublePriceAuction.deployed();
       await doublePriceAuctionInstance.registerBid(Math.floor(Math.random() * 1000), 59, {from: accounts[i]});
@@ -18,7 +18,7 @@ contract('DoublePriceAuction -> Test MCP', (accounts) => {
       let tx = await doublePriceAuctionInstance.mcp();
       let spend_time = new Date().getTime() - spend.getTime()
       let aux = (i+1).toString() + ';' + spend_time.toString() + ';' + tx.receipt.gasUsed.toString() + '\r\n';
-      fs.appendFile('mcp.csv', aux, err => {
+      fs.appendFile('results/base/mcp.csv', aux, err => {
         if (err) {
           console.error(err)
         }
